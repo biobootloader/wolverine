49c6173c9b5216da0eff7d8fa4340e27b72be2d5
Alessandro Annini <alessandro.annini@gmail.com>
2023-04-14 14:43:32 +0200

    chore: add buggy javascript file to test new feature

---
+++ buggy_script.js
@@ -0,0 +1,25 @@
+const subtractNumbers = (a, b) => a - b;
+
+const multiplyNumbers = (a, b) => a * b;
+
+const divideNumbers = (a, b) => a / b;
+
+function calculate(operation, num1, num2) {
+  let result = '';
+  if (operation == 'add') {
+    result = addNumbers(num1, num2);
+  } else if (operation == 'subtract') {
+    result = subtractNumbers(num1, num2);
+  } else if (operation == 'multiply') {
+    result = multiplyNumbers(num1, num2);
+  } else if (operation == 'divide') {
+    result = divideNumbers(num1, num2);
+  } else {
+    console.log('Invalid operation');
+  }
+
+  return res;
+}
+
+const [, , operation, num1, num2] = process.argv;
+calculate(operation, num1, num2);
